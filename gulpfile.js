5fa5474c4cd1aad56c27efe554051c11b34afc8f
Juan Carlos <klaufel@gmail.com>
2017-03-06 17:03:56 +0100

    Change task

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ gulp.task('sass', function () {
 		.pipe(mergeMediaQueries())
 		.pipe(cleanCss())
 		.pipe(gulp.dest(paths.dist))
-		.pipe(notify({ message: 'Task SASS. Compiled successful. Happy Code!', onLast: true}));
+		.pipe(notify({ message: 'Task SASS. Compiled successful.', onLast: true}));
 });
 
 // Rerun the task when a file changes
